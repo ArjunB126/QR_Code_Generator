3a21582d40397df6a16b2b660b5f6509583dfb86
ArjunB126 <arjunbajare86@gmail.com>
2024-01-31 14:03:46 +0530

    Function To generate Qr

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 let imgBox = document.getElementById("img_box");
 let QrImg = document.getElementById("qr_img");
 let QrText = document.getElementById("qr_text");
+
+
+function generateQr () 
+{
+ 
+    if (QrText.value.length > 0)
+    {
+
+       QrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+ QrText.value
+
+   }
+
+
+
+
+
+
+}
